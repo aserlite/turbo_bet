431c3535cc9235361575483c0f0d8a5f4e478ffc
aserlite <arthurcuvillon691@gmail.com>
2024-04-26 09:44:03 +0200

    templates for login

--- Escargot/urls.py
+++ Escargot/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('race', views.race, name='race'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
